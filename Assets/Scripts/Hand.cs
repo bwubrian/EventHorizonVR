589056af21fe2d0ce7d4409049880c2d9448565f
Jerry Ding <jerryding2022@berkeley.edu>
2018-11-28 15:23:29 -0800

    add VRTeleporter package and work on hand script

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+    public OVRInput.Controller controller;
+    public VRTeleporter teleporter;
+
+    public bool primaryTouched = false;
+    public bool primaryDown = false;
+    public bool previousPrimaryTouched = false;
+    public bool previousPrimaryDown = false;
+
+    // Update is called once per frame
+    void Update () {
+        primaryTouched = OVRInput.Get(OVRInput.Touch.One, controller);
+		primaryDown = OVRInput.Get(OVRInput.Button.One, controller);
+        print("Touched: " + primaryTouched + "Presssed: " + primaryDown);
+
+        if (!previousPrimaryTouched && primaryTouched)
+        {
+            teleporter.ToggleDisplay(true);
+        }
+
+        if (previousPrimaryTouched && !primaryTouched)
+        {
+            teleporter.ToggleDisplay(false);
+        }
+
+        if (!previousPrimaryDown && primaryDown)
+        {
+
+        }
+
+        previousPrimaryTouched = primaryTouched;
+        previousPrimaryDown = primaryDown;
+	}
+}
