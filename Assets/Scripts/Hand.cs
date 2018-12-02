4395a62d7625e72e3f775bf98a5c4b3036503eda
Jerry Ding <jerryding2022@berkeley.edu>
2018-12-02 13:41:30 -0800

    work on grabbing and throwing objects

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -13,21 +13,58 @@ public class Hand : MonoBehaviour {
     public bool previousPrimaryTouched = false;
     public bool previousPrimaryDown = false;
 
+    public float handTriggerState;
+    private bool grabbingObject = false;
+    private GameObject grabbedObject;
+
     void OnTriggerStay(Collider other)
     {
-        Debug.Log("Detected collision");
-        Debug.Log(other.CompareTag("Button"));
-        Debug.Log(gameplayController.stage);
+
         if (other.CompareTag("Button") && gameplayController.stage == 4)
         {
             gameplayController.stage++;
         }
+
+        if (other.CompareTag("Interactable"))
+        {
+            if (handTriggerState > 0.9f && !grabbingObject)
+            {
+                Grab(other.gameObject);
+            }
+        }
+
+    }
+
+    void Grab(GameObject obj)
+    {
+        grabbingObject = true;
+        grabbedObject = obj;
+        grabbedObject.transform.parent = transform;
+
+        grabbedObject.GetComponent<Rigidbody>().useGravity = false;
+        grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void Release()
+    {
+        grabbedObject.transform.parent = null;
+
+        Rigidbody rigidbody = grabbedObject.GetComponent<Rigidbody>();
+
+        rigidbody.useGravity = true;
+        rigidbody.isKinematic = false;
+
+        rigidbody.velocity = OVRInput.GetLocalControllerVelocity(controller);
+
+        grabbingObject = false;
+        grabbedObject = null;
     }
 
     // Update is called once per frame
     void Update () {
         primaryTouched = OVRInput.Get(OVRInput.Touch.One, controller);
 		primaryDown = OVRInput.Get(OVRInput.Button.One, controller);
+        handTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller);
         //print("Touched: " + primaryTouched + "Presssed: " + primaryDown);
 
         if (!previousPrimaryTouched && primaryTouched)
@@ -48,5 +85,13 @@ public class Hand : MonoBehaviour {
         previousPrimaryTouched = primaryTouched;
         previousPrimaryDown = primaryDown;
 
-	}
+        if (grabbingObject)
+        {
+            if (handTriggerState < 0.9f)
+            {
+                Release();
+            }
+        }
+
+    }
 }
